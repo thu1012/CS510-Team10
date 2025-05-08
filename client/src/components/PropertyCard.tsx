02a857f3e64e73cc202f2450e946da78403a0903
guonianze <nianzeg2@illinois.edu>
2025-05-08 18:42:59 -0500

    Add rankingScore to PropertyCard and PropertyDetail components

--- client/src/components/PropertyCard.tsx
+++ client/src/components/PropertyCard.tsx
@@ -1,29 +1,38 @@
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
-import { Property } from '../types/Property'
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { Property } from '../types/Property';
 
-const PropertyCard = ({ property }: { property: Property }) => {
-  const location = useLocation()
+interface PropertyWithRanking extends Property {
+  rankingScore?: number;
+}
+
+const PropertyCard = ({ property }: { property: PropertyWithRanking }) => {
+  const location = useLocation();
 
   return (
     <Link
       to={{
         pathname: `/property/${encodeURIComponent(property.id)}`,
-        search: location.search
+        search: location.search,
       }}
       style={{ textDecoration: 'none', color: 'inherit' }}
     >
       <div style={{ border: '1px solid #ccc', padding: '1rem', cursor: 'pointer' }}>
         <h4>{property.formattedAddress}</h4>
-        <p>{property.propertyType} | {property.bedrooms} bd / {property.bathrooms} ba</p>
+        <p>
+          {property.propertyType} | {property.bedrooms} bd / {property.bathrooms} ba
+        </p>
         {property.price !== null && property.price !== undefined && (
           <p>Price: ${property.price.toLocaleString()}</p>
         )}
         {property.rentalYield && <p>Rental Yield: {property.rentalYield}%</p>}
         {property.investmentScore && <p>Investment Score: {property.investmentScore}/10</p>}
+        {property.rankingScore !== null && property.rankingScore !== undefined && (
+          <p>Ranking Score: {property.rankingScore.toFixed(2)}</p>
+        )}
       </div>
     </Link>
-  )
-}
+  );
+};
 
-export default PropertyCard
+export default PropertyCard;
